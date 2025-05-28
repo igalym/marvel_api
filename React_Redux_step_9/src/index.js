70bf9091b568b8f63b652ec615eebe782fb81e60
galym <galymsakitzhan@gmail.com>
2025-05-29 03:39:21 +0500

    add project

---
+++ React_Redux_step_9/src/index.js
@@ -0,0 +1,18 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+import "./style/style.scss";
+
+
+const marvelService = new MarvelService();
+
+// marvelService.getCharacter(1011052).then(
+//     res => console.log(res)
+// )
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ React_Redux_step_9/src/services/MarvelService.js
@@ -0,0 +1,25 @@
+
+class MarvelService {
+     _apiBase = `https://gateway.marvel.com:443/v1/public/`;
+     _apiKey = `apikey=c5d6fc8b83116d92ed468ce36bac6c62`;
+
+    getResourse  = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status : ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
