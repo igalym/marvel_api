66e4b975d01f2683fff7640a57705c8e85d0ac8b
galym <galymsakitzhan@gmail.com>
2025-05-30 02:47:45 +0500

    add project update version with random character

--- React_Redux_step_9/src/services/MarvelService.js
+++ React_Redux_step_9/src/services/MarvelService.js
@@ -1,7 +1,7 @@
 
 class MarvelService {
      _apiBase = `https://gateway.marvel.com:443/v1/public/`;
-     _apiKey = `apikey=c5d6fc8b83116d92ed468ce36bac6c62`;
+     _apiKey = `apikey=c5d6fc8b83116d92ed468ce36bac6c62`;                                            
 
     getResourse  = async (url) => {
         let res = await fetch(url);
@@ -13,12 +13,25 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res =  await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async(id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    _transformCharacter = (char) => {
+        return{
+            name : char.name,
+            description : char.description,
+            thumbnail : char.thumbnail.path + '.' + char.thumbnail.extension, 
+            homepage : char.urls[0].url, 
+            wiki : char.urls[1].url,
+            comics : char.comics.items
+        }
     }
 }
 
